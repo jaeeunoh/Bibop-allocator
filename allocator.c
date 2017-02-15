4adda862fdbc2b583c2ac1c86206ad26a89aed91
Yazan Kittaneh <kittaneh17@grinnell.edu>
2017-02-15 17:01:58 -0600

    starting the malloc implementation

--- allocator.c
+++ allocator.c
@@ -18,10 +18,19 @@
 // The size of a single page of memory, in bytes
 #define PAGE_SIZE 0x1000
 
+// Linked List
+typedef struct node {
+  void* pointer;
+  node* next;
+} node_t; 
+
+node_t* freelist = NULL; 
+
 // USE ONLY IN CASE OF EMERGENCY
 bool in_malloc = false;           // Set whenever we are inside malloc.
 bool use_emergency_block = false; // If set, use the emergency space for allocations
 char emergency_block[1024];       // Emergency space for allocating to print errors
+void** headerPointer;
 
 /**
  * Allocate space on the heap.
@@ -30,8 +39,9 @@ char emergency_block[1024];       // Emergency space for allocating to print err
  *              This function may return NULL when an error occurs.
  */
 void* xxmalloc(size_t size) {
-
-  return (void*)-1;
+  int logbasesize = logbaserounder (size) - 4; 
+    
+  //return (void*)-1;
   
   // Before we try to allocate anything, check if we are trying to print an error or if
   // malloc called itself. This doesn't always work, but sometimes it helps.
@@ -48,50 +58,69 @@ void* xxmalloc(size_t size) {
   
   // Round the size up to the next multiple of the page size
   size = ROUND_UP(size, PAGE_SIZE);
-  
-  // Request memory from the operating system in page-sized chunks
-  void* p = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 
-  // Check for errors
-  if(p == MAP_FAILED) {
-    use_emergency_block = true;
-    perror("mmap");
-    exit(2);
+   
+
+  if (headerPointer[logbasesize] == NULL) {
+    // Request memory from the operating system in page-sized chunks
+    void* p = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    void* header = p;
+
+    *header = rounder(logbasesize);
+    void* next = header[rounder(logbasesize)];
+    void* nextfree = next; 
+    header[4] = next;
+    header[5] = nextfree;
+
+    for (int i = 5; i < 4048; i+= rounder(logbasesize)) {
+      void* next = header[i];
+      header[i-1] = next;  
+    }
+
+    // Check for errors
+    if(p == MAP_FAILED) {
+      use_emergency_block = true;
+      perror("mmap");
+      exit(2);
+    }
+  
+    // Done with malloc, so clear this flag
+    in_malloc = false;
+  
+    return p;
   }
-  
-  // Done with malloc, so clear this flag
-  in_malloc = false;
-  
-  return p;
-}
 
-/**
- * Free space occupied by a heap object.
- * \param ptr   A pointer somewhere inside the object that is being freed
- */
-void xxfree(void* ptr) {
-  // Not yet implemented!
-}
-
-/**
- * Get the available size of an allocated object
- * \param ptr   A pointer somewhere inside the allocated object
- * \returns     The number of bytes available for use in this object
- */
-size_t xxmalloc_usable_size(void* ptr) {
-  // We aren't tracking the size of allocated objects yet, so all we know is that it's at least PAGE_SIZE bytes.
-  //return PAGE_SIZE;
-  return 16; 
-}
-
-size_t logbaserounder (size_t n) {
-
-  size_t leading = __builtin_clzl(n);
-  size_t following = __builtin_ctzl(n);
-
-  if ((leading + following + 1) == 8*sizeof(size_t)) {
-    return following;
-  } else if ((leading + following + 1) < 8*sizeof(size_t)) {
-    return 8*sizeof(size_t) - leading;
+  /**
+   * Free space occupied by a heap object.
+   * \param ptr   A pointer somewhere inside the object that is being freed
+   */
+  void xxfree(void* ptr) {
+    // Not yet implemented!
   }
-}
+
+  /**
+   * Get the available size of an allocated object
+   * \param ptr   A pointer somewhere inside the allocated object
+   * \returns     The number of bytes available for use in this object
+   */
+  size_t xxmalloc_usable_size(void* ptr) {
+    // We aren't tracking the size of allocated objects yet, so all we know is that it's at least PAGE_SIZE bytes.
+    //return PAGE_SIZE;
+    return 16; 
+  }
+
+  size_t logbaserounder (size_t n) {
+
+    size_t leading = __builtin_clzl(n);
+    size_t following = __builtin_ctzl(n);
+
+    if ((leading + following + 1) == 8*sizeof(size_t)) {
+      return following;
+    } else if ((leading + following + 1) < 8*sizeof(size_t)) {
+      return 8*sizeof(size_t) - leading;
+    }
+  }
+
+  size_t rounder (size_t n) {
+    return 1 <<n;
+  }
