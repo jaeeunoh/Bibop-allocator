dc2d3b39b5b26c3e045e5ca3aaf406cae25e94d8
jaeeunoh <jaeeunoh@JaeEuns-MacBook-Air.local>
2017-02-20 11:59:43 -0600

    handling large memory

--- allocator.c
+++ allocator.c
@@ -31,15 +31,16 @@ typedef struct freelist_node {
 
 typedef struct header {
   size_t size;
+  size_t magic_number; 
   freelist_t* freelist;
   struct header* next;
 } header_t; 
 
 
 // USE ONLY IN CASE OF EMERGENCY
-bool in_malloc = false;           // Set whenever we are inside malloc.
-bool use_emergency_block = false; // If set, use the emergency space for allocations
-char emergency_block[1024];       // Emergency space for allocating to print errors
+//bool in_malloc = false;           // Set whenever we are inside malloc.
+//bool use_emergency_block = false; // If set, use the emergency space for allocations
+//char emergency_block[1024];       // Emergency space for allocating to print errors
 
 // List of header pointers 
 header_t* headerPointerList[8];
@@ -51,8 +52,13 @@ header_t* headerPointerList[8];
  *              This function may return NULL when an error occurs.
  */
  void* xxmalloc(size_t size) {
+  if (size > 2048) { 
+    size = ROUND_UP(size, PAGE_SIZE);
+    void* p = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+  }
+
   int logbase = logbaserounder (size); 
-  int headerlistIndex = logbaserounder (size) - 4; 
+  int headerlistIndex = logbase - 4; 
   
   size = exponent(logbase); 
 
@@ -101,6 +107,7 @@ void* allocatePage (size_t size) {
   // Initializing header 
   header_t* header = (header_t*) p;
   header->size = size;
+  header->magic_number = 1048; 
   header->next = NULL;
   header->freelist = NULL; 
 
@@ -130,10 +137,12 @@ void* allocatePage (size_t size) {
  */
  void xxfree(void* ptr) {
 
-  /*
   size_t pageStart = roundDown((size_t) ptr, PAGE_SIZE);
-
   header_t* headertemp = (header_t*) pageStart;
+  if (headertemp->magic_number != 1048) {
+    return;
+  }
+
   size_t objectSize = headertemp->size;
   
   size_t objectStart = roundDown ((size_t) ptr, objectSize);
@@ -141,7 +150,7 @@ void* allocatePage (size_t size) {
 
   freeptr->next = headertemp->freelist;
   headertemp->freelist = freeptr; 
-  */ 
+  
   return;
 }
 
@@ -158,21 +167,22 @@ void* allocatePage (size_t size) {
 }
 
 size_t logbaserounder (size_t n) {
-
   size_t leading = __builtin_clzl(n);
   size_t following = __builtin_ctzl(n);
 
-  if ((leading + following + 1) == 8*sizeof(size_t)) {
+  if ((leading + following + 1) == 8 * sizeof(size_t)) {
     return following;
-  } else if ((leading + following + 1) < 8*sizeof(size_t)) {
-    return 8*sizeof(size_t) - leading;
+  } else if ((leading + following + 1) < 8 * sizeof(size_t)) {
+    return 8 * sizeof(size_t) - leading;
   }
 }
 
+// Returns 2^n
 size_t exponent (size_t n) {
   return 1 <<n;
 }
 
+// Round a value x down to the next multiple of y
 size_t roundDown (size_t x, size_t y) {
   size_t temp = x % y;
   return x - temp; 
